70924fd24ba8755cbac1ea6886ce147c7a1fbf31
Sfoste18 <stephen.foster1981@outlook.com>
2025-01-17 14:58:18 +0000

    SF- Initial Commit

---
+++ IndecICEiveFractals/Assets/Scripts/Uimanager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+
+
+public class Uimanager : MonoBehaviour
+{
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+
+
+}
