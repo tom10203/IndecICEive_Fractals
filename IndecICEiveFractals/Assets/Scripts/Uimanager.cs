11722df6ebf58c5fabe84552b2821cf42ed7df8a
Sfoste18 <stephen.foster1981@outlook.com>
2025-01-20 14:43:41 +0000

    SF GGJ day one commit

--- IndecICEiveFractals/Assets/Scripts/Uimanager.cs
+++ IndecICEiveFractals/Assets/Scripts/Uimanager.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
 
 
 
 public class Uimanager : MonoBehaviour
 {
-
-
+    public Toggle _toggle;
+    public Slider _slider;
+    public Button _play;
+    public Button _pause;
+    public Button _save;
+    public Button _load;
+    public Dropdown _difficulty;
+    public TMP_InputField _inputField;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,10 +27,15 @@ public class Uimanager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GetComponent<AudioSource>().volume = _slider.value / 10;
+        GetComponent<AudioSource>().enabled = _toggle.isOn;
+
     }
 
-
+    public void displayText()
+    {
+        Debug.Log(_inputField.text);
+    }
 
 
 
