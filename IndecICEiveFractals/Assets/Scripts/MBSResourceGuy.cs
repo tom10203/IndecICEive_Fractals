3b2d10968403e14c01f23906686e53235343c692
GiltGames <info@giltgames.com>
2025-01-22 10:58:33 +0000

    Day3 Guy AM
    Mrongn update

--- IndecICEiveFractals/Assets/Scripts/MBSResourceGuy.cs
+++ IndecICEiveFractals/Assets/Scripts/MBSResourceGuy.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UIElements;
 
 public class MBSResourceGuy : MonoBehaviour
 {
 
     [SerializeField] int vResouceNo;
-    [SerializeField] int vResourceScore;
+    public int vResourceScore;
     public bool isAttached = false;
     [SerializeField] Transform gBubble;
     [SerializeField] Vector3 vOffset;
@@ -33,6 +34,7 @@ public class MBSResourceGuy : MonoBehaviour
     [SerializeField] GameManager GameManager;
     public bool isInBase =false;
     [SerializeField] GameObject vSelf;
+    [SerializeField] float vDist;
 
     private void Start()
     {
@@ -46,6 +48,8 @@ public class MBSResourceGuy : MonoBehaviour
         MBSBubbleEnemyInteraction = FindFirstObjectByType<MBSBubbleEnemyInteraction>().GetComponent<MBSBubbleEnemyInteraction>();
         GuyBubble.vResourcesCarried = 0;
         GameManager = FindFirstObjectByType<GameManager>().GetComponent<GameManager>();
+
+        vSelf = gameObject;
 
         //Collider = GetComponent<SphereCollider>();    
     }
@@ -81,9 +85,13 @@ public class MBSResourceGuy : MonoBehaviour
 
         if (isInBase)
         {
+            
+            transform.parent = gBase;
+            vResourceScore = 0;
 
+          //  Debug.Log("Gets closer");
             vPosTmp = transform.localPosition;
-            float vDist = vPosTmp.magnitude;
+            vDist = vPosTmp.magnitude;
 
             if (vDist > vCentreLimit)
                 
@@ -92,15 +100,15 @@ public class MBSResourceGuy : MonoBehaviour
 
                 transform.localPosition = vPosTmp;
 
-                Debug.Log("Resource has to get closer");
+             //   Debug.Log("Resource has to get closer" + vPosTmp + "    World  "   + gBase.position);
+                
             }
 
             else
             {
-                Debug.Log("Resource Destroyed");
-                GameManager.updateUI(vResourceScore, 0);
-                Destroy(gameObject);
-
+                Debug.Log("Destroy");
+               gameObject.SetActive(false);
+              
 
             }
 
@@ -116,8 +124,51 @@ public class MBSResourceGuy : MonoBehaviour
     {
         vHitShow = other.transform;
         vHitLayer = other.gameObject.layer;
+
+        if (other.gameObject.layer == 8)
+
+        {
+
+            Debug.Log("Base Hit");
+
+            GetComponent<Collider>().enabled = false;
+
+
+            isAttached = false;
+            isInBase = true;
+            GameManager.updateUI(vResourceScore, 0);
+            vResourceScore = 0;
+            Debug.Log("Resource score is " + vResourceScore);
+            if (vResourceScore == 0)
+            { 
+                Debug.Log("Score Chnged");
+            }
+
+            transform.parent = gBase;
+
+            if (GuyBubble = null)
+            {
+                Debug.Log("No GuyBubble- why??");
+            }
+
+            else
+            {
+                Debug.Log ("Size" + GuyBubble.vSize);
+            }
+
+
+            GuyBubble.vSize = GuyBubble.vSize/vSizeChange;
+            vBubbleNewSize = GuyBubble.vSize/vSizeChange;
+
+            GuyBubble.vResourcesCarried = GuyBubble.vResourcesCarried - 1;
+            if (GuyBubble.vResourcesCarried < 0)
+            {
+                GuyBubble.vResourcesCarried = 0;
+            }
+        }
+        
         // if player touches the resource
-        if (other.gameObject.layer == 6)
+        else if (other.gameObject.layer == 6)
 
         {
             Debug.Log("Current Parent " + transform.parent);
@@ -169,6 +220,16 @@ public class MBSResourceGuy : MonoBehaviour
 
 
             }
+
+
+            
+
+               
+
+
+            
+
+
         }
     }
 
