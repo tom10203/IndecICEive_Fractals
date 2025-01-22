401820dd225e118d85c3c27e47fd6f4f0586063b
CF94 <craig.fahy94@gmail.com>
2025-01-22 10:55:50 +0000

    Revert "updated movement script"
    This reverts commit 42e656ff9f84fd614774aac4d8d86e997c06104f.

--- IndecICEiveFractals/Assets/Scripts/EnemyMovement.cs
+++ IndecICEiveFractals/Assets/Scripts/EnemyMovement.cs
@@ -3,43 +3,41 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     public float speed;
-    public float torque;
     Rigidbody rb;
 
     void Start()
-    {        
+    {
         Movement();
-        rb.AddTorque(transform.up * torque);
     }
 
     void Update()
     {
-
+        
     }
 
     public void Movement()
     {
         rb = GetComponent<Rigidbody>();
-        float randomY = Random.Range(-1f, 1f);
-        float randomX = Random.Range(-1f, 1f);
+        float randomY = Random.Range(-1, 1);
+        float randomX = Random.Range(-1, 1);
         Vector3 velocity = new Vector3(randomX, randomY, 0);
         rb.linearVelocity = velocity * speed;
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("OnTriggerCall");
+
         if (other.gameObject.CompareTag("Ceiling") || other.gameObject.CompareTag("Ground"))
         {
-            Debug.Log("Ground, Ceiling trigger");
-            float y = rb.linearVelocity.y;
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, -y, 0);
-        }
-        else if (other.gameObject.CompareTag("WallLeft") || other.gameObject.CompareTag("WallRight"))
-        {
-            Debug.Log("Left, Right wall trigger");
-            float x = rb.linearVelocity.x;
-            rb.linearVelocity = new Vector3(-x, rb.linearVelocity.y, 0);
+            float Y = rb.linearVelocity.y;
+            Vector3 velocity = new Vector3(rb.linearVelocity.x, -Y, 0);
         }
 
+        if (other.gameObject.CompareTag("WallLeft") || other.gameObject.CompareTag("WallRight"))
+        {
+            float X = rb.linearVelocity.x;
+            Vector3 velocity = new Vector3(-X, rb.linearVelocity.y, 0);
+        }
     }
 }
