50ab4e260575657624b2eeabc6c77b75565428c1
CF94 <craig.fahy94@gmail.com>
2025-01-23 10:30:52 +0000

    consolidated movementY script

--- IndecICEiveFractals/Assets/Scripts/MovementY.cs
+++ IndecICEiveFractals/Assets/Scripts/MovementY.cs
@@ -1,60 +1,39 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class MovementY : MonoBehaviour
 {
-    GameObject enemyPrefab;
     public float speed = 5f;
     public float torque = 500f;
 
-    public float positiveY = 20f;
-    public float negativeY = 0f;
-
-    public bool moveUp;
-    public bool moveDown;
-
     private Rigidbody rb;
+    private bool movingUp = true;
+
     private void Start()
     {
-        moveUp = true;
         rb = GetComponent<Rigidbody>();
         rb.AddTorque(transform.up * torque);
-
     }
+
     private void Update()
-    {        
-        if (moveUp == true)
-        {
-            MoveUp();
-        }
-
-        else
-        {
-            MoveDown();
-        }
+    {
+        Move();
     }
 
-    void MoveUp()
+    private void Move()
     {
-        transform.Translate(Vector3.up * speed * Time.deltaTime);
-    }
-    void MoveDown()
-    {
-        transform.Translate(Vector3.down * speed * Time.deltaTime);
+        Vector3 direction = movingUp ? Vector3.up : Vector3.down;
+        transform.Translate(direction * speed * Time.deltaTime);
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.CompareTag("Ceiling"))
+        if (other.gameObject.CompareTag("Ceiling"))
         {
-            moveDown = true;
-            moveUp = false;
+            movingUp = false;
         }
-        
-        if (collision.gameObject.CompareTag("Ground"))
+        else if (other.gameObject.CompareTag("Ground"))
         {
-            moveDown = false;
-            moveUp = true;
+            movingUp = true;
         }
     }
 }
